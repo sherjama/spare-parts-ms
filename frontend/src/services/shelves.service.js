ddc4f9ab1e0687a5575a29352a5ede7cf81ca2ec
sherjama <sherjama8769214377@gmail.com>
2025-02-14 12:02:11 +0530

    services complete

---
+++ frontend/src/services/shelves.service.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+class Shelves {
+  shelveRoute;
+
+  constructor() {
+    this.shelveRoute = "/api/v1/shelf";
+  }
+
+  async createShelve({ shelfName }) {
+    try {
+      return await axios.post(`${this.shelveRoute}/create-shelf`, {
+        shelfName,
+      });
+    } catch (error) {
+      console.log("createShelve :", error);
+    }
+  }
+
+  async updateShelveName({ oldShelfName, newShelfName }) {
+    try {
+      return await axios.patch(`${this.shelveRoute}/update-shelf-name`, {
+        oldShelfName,
+        newShelfName,
+      });
+    } catch (error) {
+      console.log("updateShelveName :", error);
+    }
+  }
+
+  async deleteShelve(shelfName) {
+    try {
+      return await axios.delete(`${this.deleteShelve}/delete-shelf`, {
+        shelfName,
+      });
+    } catch (error) {
+      console.log("deleteShelve :", error);
+    }
+  }
+}
+
+export const shelvesService = new Shelves();
